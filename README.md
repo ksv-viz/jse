fafb5f83b759ef3d609cb84df8e9876cef7cf0fb
Serg V. Kazakov <serg.v.kazakov@mail.ru>
2020-07-16 16:13:06 +0500

    Реализованы главная петля приложения & обработка ввода с клавиатуры

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # Task manager:
 * Управление списком задач
 
-# Sofware:
-java & maven
+# Software:
+Product | Version
+--- | ---
+*java* | `11`
+*maven* | `3.6`
+
 # Developer
  serg.v.kazakov
  * serg.v.kazakov@mail.ru
 
-#Build:
+# Build:
+```
  mvn clean package install 
-#Run:
+```
+
+# Run:
+```
  java -jar task-manager-1.0.0.jar
+```
--- src/main/java/ru/ksv/tm/App.java
+++ src/main/java/ru/ksv/tm/App.java
@@ -1,5 +1,7 @@
 package ru.ksv.tm;
 
+import java.util.Scanner;
+
 import static ru.ksv.tm.constant.TerminalConst.*;
 
 public class App {
@@ -7,43 +9,68 @@ public class App {
     public static void main(final String[] args) {
         run(args);
         displayWelcome();
+        process();
     }
 
-    private static void run(final String[] args) {
+    private static void run (final String[] args) {
         if (args == null) return;
         if (args.length < 1) return;
         final String param = args[0];
+        final int  result = run(param);
+        System.exit(result);
+    }
+
+    private static int run (final String param) {
+        if (param == null || param.isEmpty()) return -1;
         switch (param) {
-            case VERSION: displayVersion();
-            case ABOUT: displayAbout();
-            case HELP: displayHelp((byte)0);
-            default: displayError(param);
+            case VERSION: return displayVersion();
+            case ABOUT: return displayAbout();
+            case HELP: return displayHelp ();
+            case EXIT: return exit();
+            default: return displayError(param);
         }
     }
 
-    private static void displayVersion() {
-        System.out.println("1.0.0.1");
-        System.exit(0);
+    private static void process() {
+        final Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (!EXIT.equals(command)) {
+            command = scanner.nextLine();
+            run(command);
+            System.out.println();
+        }
     }
 
-    private static void displayAbout() {
+    private static int exit() {
+        System.exit(0);
+        return 0;
+    }
+
+    private static int displayVersion() {
+        System.out.println("1.0.0.1");
+        return 0;
+    }
+
+    private static int displayAbout() {
         System.out.println("Serg V. Kazakov");
         System.out.println("serg.v.kazakov@mail.ru");
-        System.exit(0);
+        return 0;
     }
 
 
-    private static void displayHelp(final byte exitcode) {
+    private static int displayHelp() {
         System.out.println("version - Display application version.");
         System.out.println("about - Display developer info.");
         System.out.println("help - Display list of commands.");
-        System.exit(exitcode);
+        System.out.println("exit - Terminate console application.");
+        return 0;
     }
 
-    private static void displayError(final String errorparam) {
+    private static int displayError(final String errorparam) {
         System.out.println("Error! Unknown [" + errorparam + "] program argument...");
         System.out.println("Try one of this:");
-        displayHelp( (byte)-1);
+        displayHelp();
+        return -1;
     }
 
     private static void displayWelcome() {
